f6be0c95d9c3832add5f5626678acb28c0db6db0
lvoidi <roarba011@gmail.com>
2024-05-17 23:38:17 -0600

    Creada clase

--- backend.py
+++ backend.py
@@ -1,3 +1,20 @@
 
 class PaintxelCanvas:
-    pass
+    def __init__(self, screen: list) -> None:
+        """
+        Esta clase permite controlar de manera sencilla la matriz
+        del editor
+        """
+        # Pantalla que muestra
+        self.screen = screen
+        
+        # Historial de cambios a la pantalla 
+        self.history = []
+        self.last = 0 
+        
+        # Esta es la pantalla modificada actual. Esta se agrega a self.history 
+        # cada vez que hay un cambio
+        self.modified_screen = []
+
+
+
