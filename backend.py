0715d90c79230171f61cb5718d6a3ae613ec429f
lvoidi <roarba011@gmail.com>
2024-05-17 23:53:29 -0600

    Creada función de la transpuesta

--- backend.py
+++ backend.py
@@ -15,6 +15,28 @@ class PaintxelCanvas:
         # Esta es la pantalla modificada actual. Esta se agrega a self.history 
         # cada vez que hay un cambio
         self.modified_screen = []
+    
+    def get_transposed(self) -> list:
+        rows, columns = len(self.screen), len(self.screen[0])
+        new = [[0]*rows for _ in range(columns)]
+        for i in range(rows):
+            for j in range(columns):
+                new[j][i] = self.screen[i][j]
+        return new
 
+# Interfaz para probar el backend, testeo de la traspuesta
+if __name__ == "__main__":
+    screen = PaintxelCanvas(
+        screen=[
+            [1, 2, 5, 6, 1],
+            [3, 2, 5, 6, 1],
+            [2, 2, 6, 6, 1],
+            [1, 3, 5, 6, 1],
+        ]
+    )
 
+    print(f"""
+Matriz: {screen.screen},
+Transpuesta: {screen.get_transposed()}
+    """)
 
